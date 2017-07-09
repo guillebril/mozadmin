e67ff6049107489c5d172363373c27f04e3a9001
goshoc <shocron.diego@gmail.com>
2017-07-09 20:10:08 -0300

    algun acomodo de codigo

--- src/components/menu/itemp.js
+++ src/components/menu/itemp.js
@@ -18,9 +18,7 @@ export default class Itemp extends Component {
     };
   }
   onGestionarEdicion = (nuevo) =>{
-  
     this.props.onGestionarEdicion(
-
       //el valor del elemento nombre del elemeto
       this.props.value.nombre,
       //el valor nuevo valor del elemento
@@ -33,117 +31,105 @@ export default class Itemp extends Component {
   }
 
   onGestionarDisponibilidad (disponibilidad){
-  console.log(disponibilidad);
+    console.log(disponibilidad);
     this.props.onGestionarDisponibilidad (disponibilidad,   this.props.value.pos)
   }
 
   onBorrar = (key) =>{
     this.props.onBorrar(key)
-
   }
 
   render(
   gestionarApertura = () =>{
     this.setState({editando: !this.state.editando})
-
-
-   }
-
-  ) {
-    return(
-
-      <div>
-
-      <ListItem
-        initiallyOpen={this.state.editando}
-        onNestedListToggle={gestionarApertura}
-        secondaryTextLines={2}
-        disabled={true}
-        primaryTogglesNestedList={true}
-        className='editar_producto'
-        nestedLevel={0}
-        hoverColor={'rgba(0,0,0,0)'}
-        primaryText={
-
-
-          this.state.editando ?
-
-          <div className="menu_editar_producto">
-            <div className="item_editar_nombre">
-              <TextField
-              fullWidth={true}
-              value={this.props.value.nombre}
-              onChange={this.onGestionarEdicion}
-              name='nombre'/>
-            </div>
-            <div className="item_editar_descripcion">
-              <TextField
-              id='descripcion'
-              style={{color: '#888', fontSize: '14px' ,lineHeight: '20px'  }}
-              fullWidth={true}
-              value={this.props.value.descripcion}
-              onChange={this.onGestionarEdicion}
-              multiLine={true}
-              rows={2}
-              name='descripcion'
-              />
-            </div>
-            <div className="item_editar_precio"> $
-              <TextField
-              name="precio"
-              value={this.props.value.precio}
-              onChange={this.onGestionarEdicion}
-              fullWidth={true}
-              />
-            </div>
-            <div className="item_eliminar_producto">
-              <IconButton
-                  tooltip="Borrar"
-                  onTouchTap={() => this.onBorrar(this.props.value.key)}
-                  >
-                <ActionDelete/>
-                </IconButton>
-
-            </div>
-
-            <div>
-              <Toggle
-                 label="disponible"
-
-                 name='disponibilidad'
-                 toggled={this.props.value.disponibilidad}
-                 onToggle={()=> this.onGestionarDisponibilidad(this.props.value.disponibilidad)}
-
-               />
-            </div>
-          </div>
-
-          :
-
-          <span>
-            {this.props.value.nombre}
-            <div className="menu_producto" >
-              <div className="item_descripcion">
-              {this.props.value.descripcion}
-                </div>
-              <div className="item_precio" >
-              ${this.props.value.precio}
-              </div>
-          </div>
-          </span>
-          }
-
-        nestedItems={[
+   })
+    {
+      return(
+        <div>
           <ListItem
-            style={{marginLeft: '0', paddingTop: '0'}}
-            key={1}
+            initiallyOpen={this.state.editando}
+            onNestedListToggle={gestionarApertura}
+            secondaryTextLines={2}
             disabled={true}
-          />,
-      ]}>
-        </ListItem>
-          <Divider/>
-      </div>
+            primaryTogglesNestedList={true}
+            className='editar_producto'
+            nestedLevel={0}
+            hoverColor={'rgba(0,0,0,0)'}
+            primaryText={
 
+            this.state.editando ?
+
+            <div className="menu_editar_producto">
+              <div className="item_editar_nombre">
+                <TextField
+                fullWidth={true}
+                value={this.props.value.nombre}
+                onChange={this.onGestionarEdicion}
+                name='nombre'/>
+              </div>
+              <div className="item_editar_descripcion">
+                <TextField
+                id='descripcion'
+                style={{color: '#888', fontSize: '14px' ,lineHeight: '20px'  }}
+                fullWidth={true}
+                value={this.props.value.descripcion}
+                onChange={this.onGestionarEdicion}
+                multiLine={true}
+                rows={2}
+                name='descripcion'
+                />
+              </div>
+              <div className="item_editar_precio"> $
+                <TextField
+                name="precio"
+                value={this.props.value.precio}
+                onChange={this.onGestionarEdicion}
+                fullWidth={true}
+                />
+              </div>
+              <div className="item_eliminar_producto">
+                <IconButton
+                    tooltip="Borrar"
+                    onTouchTap={() => this.onBorrar(this.props.value.key)}
+                    >
+                  <ActionDelete/>
+                  </IconButton>
+              </div>
+              <div>
+                <Toggle
+                   label="disponible"
+                   name='disponibilidad'
+                   toggled={this.props.value.disponibilidad}
+                   onToggle={()=> this.onGestionarDisponibilidad(this.props.value.disponibilidad)}
+                 />
+              </div>
+            </div>
+
+            :
+
+            <span>
+              {this.props.value.nombre}
+              <div className="menu_producto" >
+                <div className="item_descripcion">
+                  {this.props.value.descripcion}
+                </div>
+                <div className="item_precio">
+                  ${this.props.value.precio}
+                </div>
+              </div>
+            </span>
+            }
+
+          nestedItems={[
+            <ListItem
+              style={{marginLeft: '0', paddingTop: '0'}}
+              key={1}
+              disabled={true}
+            />,
+          ]}>
+            </ListItem>
+            <Divider/>
+      </div>
     )
   }
 }
