82610bdfcebb7597b3bfe671a28bd9e7914d245f
Guille <guillebril@gmail.com>
2017-07-22 13:47:40 -0300

    después del un update

---
+++ src/components/menu/listaProductos.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { SortableContainer, } from 'react-sortable-hoc';
+import { List } from 'material-ui/List';
+import Subheader from 'material-ui/Subheader';
+import FlatButton from 'material-ui/FlatButton';
+
+import TituloCategoria from './tituloCategoria';
+import ProductoContenedor from './productoContenedor'
+
+//Este compoenente genera la categoria
+const ListaProductos = SortableContainer(({ items, categoria ,nombreCategoria, onGestionarEdicion, onGestionarDisponibilidad , onGestionarEdicionCategoria, agregarProducto, onBorrar }) => {
+//Muestra la lista
+  return(
+    <List>
+      <Subheader>
+        <TituloCategoria
+        categoria={categoria}
+        onGestionarEdicionCategoria={onGestionarEdicionCategoria}
+        nombreCategoria={nombreCategoria}/>
+      </Subheader>
+
+      {
+        items.map((value, index) => (
+            <ProductoContenedor
+              key={value.key}
+              index={index}
+              value={value}
+              onBorrar={onBorrar}
+              onGestionarEdicion={onGestionarEdicion}
+              onGestionarDisponibilidad={onGestionarDisponibilidad}
+               />
+        ))}
+        <br/>
+        <FlatButton label="Agregar Producto" onTouchTap={agregarProducto} primary={true} />
+      </List>
+  );
+});
+
+
+export default ListaProductos
